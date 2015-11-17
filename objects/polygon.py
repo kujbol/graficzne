bdbdec96bddc3d5281a499cead7c3981c65f7f03
kujbol <kujbol@gmail.com>
2015-11-17 12:34:02 +0100

    long way to working polygon intersection

--- objects/polygon.py
+++ objects/polygon.py
@@ -1,8 +1,25 @@
+from itertools import chain
+from calculations.basics import Segment
 from draw.line import draw_line, draw_line_anty_aliasing
 from draw.polygon import clean_polygon_inside, re_draw_polygon_inside
 
 from objects.basics import Settings, BasicPointClass
 from objects.point import Point
+
+
+def add_inter_points(graph, intersection_points, segments):
+    for segment in segments:
+        polygon_intersection_points = []
+        for intersection_point in intersection_points:
+            if segment.direction(intersection_point) == 0:
+                polygon_intersection_points.append(intersection_point)
+        polygon_intersection_points.sort(
+            key=lambda p: segment.p1.distance(p), reverse=True
+        )
+        previous = segment.p1
+        for point in polygon_intersection_points:
+            graph[previous].apend(point)
+        graph[point] = segment.p2
 
 
 class Polygon(BasicPointClass):
@@ -67,3 +84,39 @@ class Polygon(BasicPointClass):
             clean_polygon_inside(self)
         else:
             re_draw_polygon_inside(self)
+
+    def find_intersection_with_polygon(self, polygon):
+        polygon_segments = [
+            Segment(p1, p2)
+            for p1, p2 in zip(polygon.points[:-1], polygon.points[1:])
+        ]
+        polygon_segments.append(Segment(polygon.points[-1], polygon.points[0]))
+
+        self_segments = [
+            Segment(p1, p2)
+            for p1, p2 in zip(self.points[:-1], self.points[1:])
+        ]
+        self_segments.append(Segment(self.points[-1], self.points[0]))
+
+        intersection_points = [
+            Segment.intersection(self_segment, segment)
+            for segment in polygon_segments
+            for self_segment in self_segments
+            if Segment.is_intersection(self_segment, segment)
+        ]
+
+        graph = {
+            segment.p1: []
+            for segment in chain(polygon_segments, self_segments)
+        }
+
+        for point in intersection_points:
+            graph[point] = []
+
+        add_inter_points(graph, intersection_points, polygon_segments)
+        add_inter_points(graph, intersection_points, self_segments)
+
+        visited = {
+            obj: False for obj in graph.iterkeys()
+        }
+
